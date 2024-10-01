94ea79756741db468a21f1be9c5a9e7c68c078ba
MaPankow <162994783+MaPankow@users.noreply.github.com>
2024-10-01 15:56:41 +0200

    Collection user_profile erstellt und Daten eingefügt

--- users_insert.mongodb.js
+++ users_insert.mongodb.js
@@ -209,3 +209,189 @@ use('planet-A');
 //     ]
 // );
 
+// db.createCollection("user_profile");
+db.user_profile.insertMany(
+    
+        [
+            {
+                "user_id": "66fab69a32a76bc5dd059e2c",
+                "username": "Max Mustermann",
+                "profiltext": "Ich liebe es, vegane Rezepte zu kreieren, besonders meine berühmten Linsensuppe.",
+                "geburtstag": "1985-03-12",
+                "social_links": {
+                    "facebook": "https://facebook.com/max.mustermann",
+                    "instagram": "https://instagram.com/max.mustermann"
+                }
+            },
+            {
+                "user_id": "66fab69a32a76bc5dd059e2d",
+                "username": "Anna Müller",
+                "profiltext": "Mein Lieblingsgericht ist veganes Chili. Es ist einfach und köstlich!",
+                "geburtstag": "1990-07-25",
+                "social_links": {
+                    "facebook": "https://facebook.com/anna.mueller"
+                }
+            },
+            {
+                "user_id": "66fab69a32a76bc5dd059e2e",
+                "username": "Peter Schmidt",
+                "profiltext": "Ich bin ein Fan von Smoothies und experimentiere gerne mit neuen Zutaten.",
+                "geburtstag": "1978-11-14",
+                "social_links": {
+                    "instagram": "https://instagram.com/peter.schmidt"
+                }
+            },
+            {
+                "user_id": "66fab69a32a76bc5dd059e2f",
+                "username": "Laura Becker",
+                "profiltext": "",
+                "geburtstag": "1995-01-30",
+                "social_links": {
+                    "facebook": "https://facebook.com/laura.becker",
+                    "pinterest": "https://pinterest.com/laura.becker"
+                }
+            },
+            {
+                "user_id": "66fab69a32a76bc5dd059e30",
+                "username": "Thomas Wagner",
+                "profiltext": "Ich genieße es, vegane Desserts zu backen, besonders Schokoladenkekse.",
+                "geburtstag": "1982-09-05",
+                "social_links": {
+                    "instagram": "https://instagram.com/thomas.wagner"
+                }
+            },
+            {
+                "user_id": "66fab69a32a76bc5dd059e31",
+                "username": "Julia Fischer",
+                "profiltext": "Ich liebe es, neue vegane Rezepte auszuprobieren und zu teilen.",
+                "geburtstag": "1989-04-20",
+                "social_links": {
+                    "facebook": "https://facebook.com/julia.fischer"
+                }
+            },
+            {
+                "user_id": "66fab69a32a76bc5dd059e32",
+                "username": "Markus Hoffmann",
+                "profiltext": "",
+                "geburtstag": "1965-12-15",
+                "social_links": {
+                    "pinterest": "https://pinterest.com/markus.hoffmann"
+                }
+            },
+            {
+                "user_id": "66fab69a32a76bc5dd059e33",
+                "username": "Sophie Schneider",
+                "profiltext": "Ich koche leidenschaftlich gerne vegane Pasta mit frischem Gemüse.",
+                "geburtstag": "1980-06-22",
+                "social_links": {
+                    "instagram": "https://instagram.com/sophie.schneider"
+                }
+            },
+            {
+                "user_id": "66fab69a32a76bc5dd059e34",
+                "username": "David Klein",
+                "profiltext": "",
+                "geburtstag": "1975-08-10",
+                "social_links": {
+                    "facebook": "https://facebook.com/david.klein"
+                }
+            },
+            {
+                "user_id": "66fab69a32a76bc5dd059e35",
+                "username": "Lisa Braun",
+                "profiltext": "Vegane Burger sind meine Leidenschaft! Ich liebe es, neue Variationen zu kreieren.",
+                "geburtstag": "1992-05-18",
+                "social_links": {
+                    "pinterest": "https://pinterest.com/lisa.braun"
+                }
+            },
+            {
+                "user_id": "66fab69a32a76bc5dd059e36",
+                "username": "Jan Richter",
+                "profiltext": "",
+                "geburtstag": "1988-02-28",
+                "social_links": {}
+            },
+            {
+                "user_id": "66fab69a32a76bc5dd059e37",
+                "username": "Nina Schwarz",
+                "profiltext": "Ich liebe es, vegane Snacks zu machen, besonders Energiebällchen.",
+                "geburtstag": "1994-10-09",
+                "social_links": {
+                    "facebook": "https://facebook.com/nina.schwarz",
+                    "instagram": "https://instagram.com/nina.schwarz"
+                }
+            },
+            {
+                "user_id": "66fab69a32a76bc5dd059e38",
+                "username": "Felix Zimmermann",
+                "profiltext": "Ich experimentiere gerne mit verschiedenen veganen Käsesorten.",
+                "geburtstag": "1972-03-03",
+                "social_links": {
+                    "instagram": "https://instagram.com/felix.zimmermann"
+                }
+            },
+            {
+                "user_id": "66fab69a32a76bc5dd059e39",
+                "username": "Klara Wolf",
+                "profiltext": "",
+                "geburtstag": "1981-07-21",
+                "social_links": {
+                    "facebook": "https://facebook.com/klara.wolf"
+                }
+            },
+            {
+                "user_id": "66fab69a32a76bc5dd059e3a",
+                "username": "Leon Weber",
+                "profiltext": "Ich liebe es, veganes Sushi zu machen und mit Freunden zu teilen.",
+                "geburtstag": "1996-11-11",
+                "social_links": {}
+            },
+            {
+                "user_id": "66fab69a32a76bc5dd059e3b",
+                "username": "Emma Wagner",
+                "profiltext": "Meine Spezialität sind vegane Muffins, die einfach unwiderstehlich sind!",
+                "geburtstag": "1993-04-27",
+                "social_links": {
+                    "pinterest": "https://pinterest.com/emma.wagner"
+                }
+            },
+            {
+                "user_id": "66fab69a32a76bc5dd059e3c",
+                "username": "Simon Hartmann",
+                "profiltext": "Ich genieße es, neue vegane Rezepte zu entwickeln und zu testen.",
+                "geburtstag": "1987-09-16",
+                "social_links": {
+                    "instagram": "https://instagram.com/simon.hartmann"
+                }
+            },
+            {
+                "user_id": "66fab69a32a76bc5dd059e3d",
+                "username": "Marie König",
+                "profiltext": "",
+                "geburtstag": "1991-01-01",
+                "social_links": {}
+            },
+            {
+                "user_id": "66fab69a32a76bc5dd059e3e",
+                "username": "Paul Schulte",
+                "profiltext": "Ich liebe es, vegane Gerichte zu kochen und neue Rezepte zu entdecken.",
+                "geburtstag": "1984-08-08",
+                "social_links": {
+                    "facebook": "https://facebook.com/paul.schulte"
+                }
+            },
+            {
+                "user_id": "66fab69a32a76bc5dd059e3f",
+                "username": "Tina Peters",
+                "profiltext": "Veganes Essen ist meine Leidenschaft, besonders frische Salate!",
+                "geburtstag": "1999-12-12",
+                "social_links": {
+                    "instagram": "https://instagram.com/tina.peters"
+                }
+            }
+        ]
+    
+);
+
+db.user_profile.find({});
